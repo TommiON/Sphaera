cf2c2d3134a4902889779d630aedef80a1f21790
TommiON <tommi.niittymies@gmail.com>
2025-03-19 21:27:02 +0200

    Ensimmäinen validaattori

--- validators/clubValidator.ts
+++ validators/clubValidator.ts
@@ -3,23 +3,19 @@ import express, {Express, Request, Response} from 'express';
 import appDataSource from '../config/datasource';
 import Club from '../entities/clubs';
 
-// nää kannattaisi varmaan keskittää jonnekin omaan palveluluokkaansa? utilities?
 const clubRepository = appDataSource.getRepository(Club);
 
-export const validateNewClub = (req: Request, res: Response, next: express.NextFunction) => {
+
+export const validateNewClub = async (req: Request, res: Response, next: express.NextFunction) => {
+    const existingClubs = await clubRepository.find({ where: { name: req.body.name }});
+
     if (!req.body.name) {
         res.status(400).json({ error: 'Nimi puuttuu'});
-    } else if (nameAlreadyUsed(req.body.name)) {
-        res.status(400).json({ error: 'Nimi on varattu'});
+        return;
+    } else if (existingClubs.length > 0) {
+        res.status(400).json({ error: 'Nimi varattu'});
+        return;
     } else {
         next();
     }
-}
-
-// tämä ei toimi
-function nameAlreadyUsed(proposedName: string): boolean {
-    let inUse: boolean = false;
-    clubRepository.find({ where: { name: proposedName } })
-        .then(response => inUse = response.length > 0);
-    return inUse;
 }
