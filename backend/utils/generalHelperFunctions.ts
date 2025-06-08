06a848bf2a83108a2b1deb309efa488ae2b0e329
TommiON <tommi.niittymies@gmail.com>
2025-06-08 18:11:05 +0300

    Backend, pieni apufunktio parillisuuden tarkistamiseen

--- backend/utils/generalHelperFunctions.ts
+++ backend/utils/generalHelperFunctions.ts
@@ -1,3 +1,7 @@
 export const transformEnumIntoStringList = (original: any): string[] => {
    return Object.values(original).filter(v => typeof v == 'string');
 }
+
+export const isEven = (entry: number): boolean => {
+   return entry % 2 == 0;
+}
